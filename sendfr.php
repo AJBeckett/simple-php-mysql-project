e7aae2b1b5c6737ca80ecfe391381b9fc4fdf98b
AJBeckett <38403344+AJBeckett@users.noreply.github.com>
2020-03-23 09:10:55 -0400

    Add files via upload

--- sendfr.php
+++ sendfr.php
@@ -1,45 +1,46 @@
 <?php
-$config=parse_ini_file('/home/abecket3/public_html/Assign4b/private/config.ini');
 Session_Start();
 if(IsSet($_SESSION["user_id"])) {
-$recv_id=$_POST["h1"];
-$recv_name=$_POST["h2"];
-$frnd_id=$_SESSION["user_id"];
-$frnd_name=$_SESSION["name"];
+	$recv_id=$_POST["h1"];
+	$recv_name=$_POST["h2"];
+	$frnd_id=$_SESSION["user_id"];
+	$frnd_name=$_SESSION["name"];
 
 
 
-			$resid=MySQLi_Connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
-			if(MySQLi_Connect_Errno()) {
-				echo "<tr align='center'> <td colspan='5'> Failed to connect to MySQL </td> </tr>";
+	$resid=MySQLi_Connect('localhost','abecket3','Sy!veon369852','abecket3');
+	if(MySQLi_Connect_Errno()) {
+		echo "<tr align='center'> <td colspan='5'> Failed to connect to MySQL </td> </tr>";
+		}
+		else {
+			$count=MySQLi_Query($resid,"select (max(id)+1) as count from friends");
+			$count_id=MySQLi_Fetch_Assoc($count);
+			
+			if($count_id["count"]) {
+			$query="insert into friends values (".$count_id["count"].",'$recv_id','$recv_name',$frnd_id,'$frnd_name',0,0)"; //Query fired in this way>>> insert into friends values(1,1,'Kunal',2,'Tron',0,0);
 			}
 			else {
-				$count=MySQLi_Query($resid,"select (max(id)+1) as count from friends");
-				$count_id=MySQLi_Fetch_Assoc($count);
-				if($count_id["count"]) {
-				$query="insert into friends values (".$count_id["count"].",'$recv_id','$recv_name',$frnd_id,'$frnd_name',0,0)"; //Query fired in this way>>> insert into friends values(1,1,'Kunal',2,'Tron',0,0);
-				}
-				else {
-				$query="insert into friends values (1,'$recv_id','$recv_name',$frnd_id,'$frnd_name',0,0)";
-				}
-				$res=MySQLi_Query($resid,$query);
-				
-				if($res) {
-			     echo "Successful!";
-				}
+			$query="insert into friends values (1,'$recv_id','$recv_name',$frnd_id,'$frnd_name',0,0)";
+			}
+			
+			$res=MySQLi_Query($resid,$query);
+			
+			if($res) {
+		     echo "Successful!";
+			}
 			else {
 				 echo "Failed!";
 			}
 				MySQLi_Close($resid);
-			}
-
+		}
+	}
 }
 					
-					if(IsSet($_SESSION['user_id'])) {
-						Header("Location: friends.php");
-						}
-					else {
-						Header("Location: home.php");
-					}
+if(IsSet($_SESSION['user_id'])) {
+	Header("Location: friends.php");
+}
+else {
+	Header("Location: home.php");
+}
 ?>
 
